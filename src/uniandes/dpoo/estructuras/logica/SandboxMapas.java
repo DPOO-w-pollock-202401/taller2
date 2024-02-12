f7cafaab26ec29fcc98fbb1fd4eaad211ccde9f7
w-pollock <w.pollock@uniandes.edu.co>
2024-02-11 23:50:05 -0500

    Entrega Taller 2

---
+++ src/uniandes/dpoo/estructuras/logica/SandboxMapas.java
@@ -0,0 +1,191 @@
+package uniandes.dpoo.estructuras.logica;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Esta clase tiene un conjunto de métodos para practicar operaciones sobre mapas.
+ *
+ * Todos los métodos deben operar sobre el atributo mapaCadenas que se declara como un Map.
+ * 
+ * En este mapa, las llaves serán cadenas y los valores serán también cadenas. La relación entre los dos será que cada llave será igual a la cadena del valor, pero invertida.
+ * 
+ * El objetivo de usar el tipo Map es que sólo puedan usarse métodos de esa interfaz y no métodos adicionales provistos por la implementación concreta (HashMap).
+ * 
+ * No pueden agregarse nuevos atributos.
+ */
+public class SandboxMapas
+{
+    /**
+     * Un mapa de cadenas para realizar varias de las siguientes operaciones.
+     * 
+     * Las llaves del mapa son cadenas, así como los valores.
+     * 
+     * Las llaves corresponden a invertir la cadena que aparece asociada a cada llave.
+     */
+    private Map<String, String> mapaCadenas;
+
+    /**
+     * Crea una nueva instancia de la clase con las dos listas inicializadas pero vacías
+     */
+    public SandboxMapas( )
+    {
+        mapaCadenas = new HashMap<String, String>( );
+    }
+
+    /**
+     * Retorna una lista con las cadenas del mapa (los valores) ordenadas lexicográficamente
+     * @return Una lista ordenada con las cadenas que conforman los valores del mapa
+     */
+    public List<String> getValoresComoLista( )
+    {
+    	List<String> valor = new java.util.ArrayList<String>(mapaCadenas.values());
+        java.util.Collections.sort(valor);
+        return valor;
+    }
+
+    /**
+     * Retorna una lista con las llaves del mapa ordenadas lexicográficamente de mayor a menor
+     * @return Una lista ordenada con las cadenas que conforman las llaves del mapa
+     */
+    public List<String> getLlavesComoListaInvertida( )
+    {
+    	List<String> keys = new java.util.ArrayList<String>(mapaCadenas.keySet());
+        java.util.Collections.sort(keys, java.util.Collections.reverseOrder());
+        return keys;
+    }
+
+    /**
+     * Retorna la cadena que sea lexicográficamente menor dentro de las llaves del mapa .
+     * 
+     * Si el mapa está vacío, debe retornar null.
+     * @return
+     */
+    public String getPrimera( )
+    {
+    	if (mapaCadenas.isEmpty()) {
+            return null;
+        }
+        return java.util.Collections.min(mapaCadenas.keySet());
+    }
+
+    /**
+     * Retorna la cadena que sea lexicográficamente mayor dentro de los valores del mapa
+     * 
+     * Si el conjunto está vacío, debe retornar null.
+     * @return
+     */
+    public String getUltima( )
+    {
+    	if (mapaCadenas.isEmpty()) {
+            return null;
+        }
+        return java.util.Collections.max(mapaCadenas.values());
+    }
+
+    /**
+     * Retorna una colección con las llaves del mapa, convertidas a mayúsculas.
+     * 
+     * El orden de las llaves retornadas no importa.
+     * @return Una lista de cadenas donde todas las cadenas están en mayúsculas
+     */
+    public Collection<String> getLlaves( )
+    {
+    	Collection<String> keys = mapaCadenas.keySet();
+        for (String key : keys) {
+            key = key.toUpperCase();
+        }
+        return keys;
+    }
+
+    /**
+     * Retorna la cantidad de *valores* diferentes en el mapa
+     * @return
+     */
+    public int getCantidadCadenasDiferentes( )
+    {
+    	int contar = 0;
+        for (String value : mapaCadenas.values()) {
+            if (mapaCadenas.values().stream().filter(v -> v.equals(value)).count() == 1) {
+                contar++;
+            }
+        }
+        return contar;
+    }
+
+    /**
+     * Agrega un nuevo valor al mapa de cadenas: el valor será el recibido por parámetro, y la llave será la cadena invertida
+     * 
+     * Este método podría o no aumentar el tamaño del mapa, dependiendo de si ya existía la cadena en el mapa
+     * 
+     * @param cadena La cadena que se va a agregar al mapa
+     */
+    public void agregarCadena( String cadena )
+    {
+    	if (!mapaCadenas.containsKey(cadena)) {
+            mapaCadenas.put(cadena, cadena);
+        }
+    }
+
+    /**
+     * Elimina una cadena del mapa, dada la llave
+     * @param cadena La llave para identificar el valor que se debe eliminar
+     */
+    public void eliminarCadenaConLLave( String llave )
+    {
+    	mapaCadenas.remove(llave);
+    }
+
+    /**
+     * Elimina una cadena del mapa, dado el valor
+     * @param cadena El valor que se debe eliminar
+     */
+    public void eliminarCadenaConValor( String valor )
+    {
+    	
+    }
+
+    /**
+     * Reinicia el mapa de cadenas con las representaciones como Strings de los objetos contenidos en la lista del parámetro 'objetos'.
+     * 
+     * Use el método toString para convertir los objetos a cadenas.
+     * @param valores Una lista de objetos
+     */
+    public void reiniciarMapaCadenas( List<Object> objetos )
+    {
+    	mapaCadenas.clear();
+        for (Object objeto : objetos) {
+            mapaCadenas.put(objeto.toString(), objeto.toString());
+        }
+    }
+
+    /**
+     * Modifica el mapa de cadenas reemplazando las llaves para que ahora todas estén en mayúsculas pero sigan conservando las mismas cadenas asociadas.
+     */
+    public void volverMayusculas( )
+    {
+    	Map<String, String> newMap = new HashMap<String, String>();
+        for (String key : mapaCadenas.keySet()) {
+            newMap.put(key.toUpperCase(), mapaCadenas.get(key));
+        }
+        mapaCadenas = newMap;
+    }
+
+    /**
+     * Verifica si todos los elementos en el arreglo de cadenas del parámetro hacen parte del mapa de cadenas (de los valores)
+     * @param otroArreglo El arreglo de enteros con el que se debe comparar
+     * @return True si todos los elementos del arreglo están dentro de los valores del mapa
+     */
+    public boolean compararValores( String[] otroArreglo )
+    {
+    	for (String value : otroArreglo) {
+            if (!mapaCadenas.values().contains(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
